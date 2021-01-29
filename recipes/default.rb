4450f348620e5028c7cca327f66c2878a48c8fd6
Gayatri Ajith <gayatri@schogini.com>
2021-01-29 18:37:25 +0000

    First push

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+windows_package 'Install MariaDB' do
+  package_name 'mariadb'
+  action :install
+  source 'https://downloads.mariadb.org/interstitial/mariadb-10.5.8/winx64-packages/mariadb-10.5.8-winx64.msi/from/https%3A//mirrors.bkns.vn/mariadb/'
+end
